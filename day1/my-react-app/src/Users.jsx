42074b7146cf48b9b0c68f599cf3615c3ff11a86
Shailesh Sonare <shailesh.sonare@netcorecloud.com>
2026-02-14 19:17:08 +0530

    react first commit

--- day1/my-react-app/src/Users.jsx
+++ day1/my-react-app/src/Users.jsx
@@ -1,13 +1,18 @@
 import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 export default function Users() {
-    const [users, setUsers] = useState([]);
+    const [users, setUsers] = useState([]); //useState hook
 
     useEffect(() => {
-        let data = [
-            {id: 1, login: "Abc", avatar_url: "xyz"}
-        ];
-        setUsers(data)
+        // let data = [
+        //     {id: 1, login: "Abc", avatar_url: "xyz"}
+        // ];
+        // setUsers(data)
+        axios.get('https://api.github.com/users')
+        .then(response => {
+            setUsers(response.data)
+        })
     }, [])
 
   return (
