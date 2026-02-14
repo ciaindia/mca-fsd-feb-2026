97644d0f244f94acea1f04d33ba7d298a842ff5d
Shailesh Sonare <shailesh.sonare@netcorecloud.com>
2026-02-14 19:20:36 +0530

    react first commit

--- day1/my-react-app/src/Users.jsx
+++ day1/my-react-app/src/Users.jsx
@@ -2,22 +2,45 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 export default function Users() {
-    const [users, setUsers] = useState([]); //useState hook
+  const [users, setUsers] = useState([])
 
-    useEffect(() => {
-        // let data = [
-        //     {id: 1, login: "Abc", avatar_url: "xyz"}
-        // ];
-        // setUsers(data)
-        axios.get('https://api.github.com/users')
-        .then(response => {
-            setUsers(response.data)
-        })
-    }, [])
+  useEffect(() => {
+    axios.get('https://api.github.com/users')
+      .then(response => {
+        setUsers(response.data)
+      })
+      .catch(error => {
+        console.error(error)
+      })
+  }, [])
 
   return (
     <div>
-        {JSON.stringify(users)}
+      <table border="1" cellPadding="10">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Username</th>
+            <th>Avatar</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {users.map(user => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.login}</td>
+              <td>
+                <img
+                  src={user.avatar_url}
+                  alt={user.login}
+                  width="50"
+                />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   )
 }
