679180ef3a53b6ee93074692a9ecbb4323abba4f
Shailesh Sonare <shailesh.sonare@netcorecloud.com>
2026-02-14 16:26:16 +0530

    day 1 app.js

--- day1/app.js
+++ day1/app.js
@@ -68,3 +68,31 @@ for(let i = 0; i < nums.length; i++) {
 
 console.log(sum);
 //CW:WAP to print square of all elements of array
+//and assign it to new array
+
+// sqr_arr = []
+
+// for(let i = 0; i < nums.length; i++)
+//     sqr_arr.push(nums[i] * nums[i])
+
+// console.log(`Sqr Arr: ${sqr_arr}`);
+
+// sqr_arr = nums.map(x => x * x)
+// console.log(sqr_arr);
+
+//CW: WAP to find cube of all elements
+//CW: WAP to find even numbers of array using filter function
+// even_arr = nums.filter(x => x % 2 == 0)
+// console.log(even_arr);
+
+members = [
+    {name: "Amit", age: 23},
+    {name: "Sumit", age: 19},
+    {name: "Suresh", age: 22},
+]
+
+allowed_members = members.filter((m) => {
+    return m.age >= 21
+})
+
+console.log(allowed_members);
