93f189a6f1de1f0603a52aa97d69e47f47bfed70
Shailesh Sonare <shailesh.sonare@netcorecloud.com>
2026-02-21 19:11:13 +0530

    day2

--- day2/dbconn.js
+++ day2/dbconn.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const mysql = require("mysql2");
+const mysql = require("mysql2");    //npm install mysql2
 
 const db = mysql.createConnection({
     host: "localhost",
