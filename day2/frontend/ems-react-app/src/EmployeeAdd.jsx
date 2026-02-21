90463ea90ab8b6dfdc3c7c443bb8746e4159c4ea
Shailesh Sonare <shailesh.sonare@netcorecloud.com>
2026-02-21 18:08:48 +0530

    day2

--- day2/frontend/ems-react-app/src/EmployeeAdd.jsx
+++ day2/frontend/ems-react-app/src/EmployeeAdd.jsx
@@ -2,25 +2,22 @@ import React, { useState } from 'react'
 import axios from 'axios'
 
 export default function EmployeeAdd() {
-
     const [name, setName] = useState("")
     const [salary, setSalary] = useState("")
 
     const addEmployee = () => {
         axios.post("http://localhost:3001/employee", { name, salary })
         alert("Employee added successfully")
-        setName("")
-        setSalary("")
+        window.location.reload()
     }
-
 
   return (
     <div>
         <h1>Add Employee</h1>
             <label htmlFor="name">Name</label>
-            <input type="text" id="name" name="name" onChange={(e) => setName(e.target.value)}/>
+            <input type="text" onChange={(e) => setName(e.target.value)}/>
             <label htmlFor="salary">Salary</label>
-            <input type="text" id="salary" name="salary" onChange={(e) => setSalary(e.target.value)}/>
+            <input type="text" onChange={(e) => setSalary(e.target.value)}/>
             <button onClick={addEmployee}>Add Employee</button>
     </div>
   )
