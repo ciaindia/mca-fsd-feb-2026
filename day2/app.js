021eb48f46c91db827cb6db3703343f8188151cf
Shailesh Sonare <shailesh.sonare@netcorecloud.com>
2026-02-21 15:07:59 +0530

    day2

--- day2/app.js
+++ day2/app.js
@@ -2,20 +2,38 @@ const express = require("express")
 const app = express()
 
 //run this on browser
-//http://localhost:3000/
+//http://localhost:3000/endpoint
 app.get("/", (req, res) => {
     res.send("Hello World!")
 })
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.get("/calc", (req, res) => {
     res.send("GET Calc")
 })
 
+//CW: POST PUT and DELETE method for /calc
+
+
+
+
+
 app.listen(3000, () => {
     console.log("Server started on port 3000")
 })
 
-//install nodemon: npm i g nodemon
+//install nodemon: npm i -g nodemon
 //run this application first in terminal 
 // node app.js
 // nodemon app.js 
