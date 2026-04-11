475e2487e7ba3969d2d42080c89d691e0d10eebb
Shailesh Sonare <ssscheems@gmail.com>
2026-04-11 16:14:23 +0530

    Add files via upload

--- day4/app.js
+++ day4/app.js
@@ -21,11 +21,36 @@ app.post("/login", (req, res) => {
             console.log(err)
         } else {
             if (result.length > 0) {
-                const token = jwt.sign({ username }, SECRETKEY, { expiresIn: "1h" })
+                const token = jwt.sign({ username }, SECRETKEY, { expiresIn: "1m" })
                 res.json({ token })
             } else {
                 res.json({ message: "Invalid credentials" })
             }
+        }
+    })
+})
+
+
+const authenticateToken = (req, res, next) => {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+ 
+    if (!token) return res.sendStatus(401)
+ 
+    jwt.verify(token, SECRETKEY, (err, user) => {     
+        if (err) return res.sendStatus(403)
+        req.user = user
+        next()
+    })
+}
+
+app.get("/tasks", authenticateToken, (req, res) => {
+    const query = "SELECT * FROM todolist"
+    db.query(query, (err, result) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.json(result)
         }
     })
 })
